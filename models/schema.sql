84e4a2b066f03fc1771b75258fe18537e6cdcad2
unknown <josh_taylor33@hotmail.com>
2019-09-14 12:20:04 -0700

    fix sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,6 +1,20 @@
+DROP DATABASE IF EXISTS bigdata_db;
+CREATE DATABASE chirpydb;
+USE chirpydb;
 
+CREATE TABLE chirp_table (
+  item_id INT(10) AUTO_INCREMENT NOT NULL,
+  bird_name varchar(255) NOT NULL,
+  family varchar(255) NOT NULL,
+  voice varchar(255) NOT NULL,
+  habitat varchar(255) NOT NULL,
+  places varchar(255) NOT NULL,
+  
+  PRIMARY KEY(item_id)
+);
+   
    SELECT * FROM chirp_table;
-      INSERT INTO chirp_table (bird_name, family, voice, habitat, location)
+      INSERT INTO chirp_table (bird_name, family, voice, habitat, places)
          VALUES 
             ("Red-Throated Loon", "Loon", "Silent in NW", "Ocean and Bays", "Coast"),
             ("Pacific Loon", "Loon", "Silent in NW", "Ocean and bays", "Coast"),
