956752296f5052a7ac1b86edd63d70a595377b1e
Nary Hang <naryhang@gmail.com>
2019-09-14 11:41:43 -0700

    update database

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,22 @@
-DROP DATABASE IF EXISTS exampledb;
 CREATE DATABASE exampledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+
+CREATE DATABASE chirpydb;
+
+USE chirpydb;
+
+CREATE TABLE chirp_table (
+  item_id INT(10) AUTO_INCREMENT NOT NULL,
+  bird_name varchar(100) NOT NULL,
+  family varchar(255) NOT NULL,
+  voice varchar(255) NOT NULL,
+  habitat varchar(255) NOT NULL,
+  location varchar(255) NOT NULL,
+  PRIMARY KEY(item_id)
+);
+   
+   SELECT * FROM chirp_table;
+   
+   INSERT INTO chirp_table (bird_name, family, voice, habitat, location)
+   VALUES ("Red-Throated Loon", "Loon", "Silent in NW", "Ocean and Bays", "Coast"),
+   ("Pacific Loon", "Loon", "Silent in NW", "Ocean and bays", "Coast");
